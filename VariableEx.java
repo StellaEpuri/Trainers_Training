44b2f3080e09189e73afa547ac3a4d043862db12
StellaEpuri <137855187+StellaEpuri@users.noreply.github.com>
2023-09-13 23:55:29 +0530

    variables and datatypes

---
+++ VariableEx.java
@@ -0,0 +1,15 @@
+package com.corejava.basics;
+
+public class VariableEx {
+
+	public static void main(String[] args) {
+		
+		double num1=100.5;//initialization
+		int num2=200;
+		double result;//declaration
+		result=num1+num2;
+
+		System.out.println(result);
+	}
+
+}
