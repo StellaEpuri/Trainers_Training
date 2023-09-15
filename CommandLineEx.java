cf68ff794dfd1df104c2fa602c15cacd088ed78f
StellaEpuri <137855187+StellaEpuri@users.noreply.github.com>
2023-09-15 21:28:24 +0530

    Add files via upload

---
+++ CommandLineEx.java
@@ -0,0 +1,15 @@
+class CommandLineEx{
+public static void main(String args[]){// 0 1
+
+//int i=10;
+//int j=20;
+
+int i=Integer.parseInt(args[0]);
+int j=Integer.parseInt(args[1]);
+
+int result=i+j;
+System.out.println("the sum:"+result);
+
+}
+
+}
